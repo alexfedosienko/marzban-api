cb81722055452b39b744ea937f481cb24c70ecda
alexfedosienko <alexfedosienko@gmail.com>
2024-12-11 19:39:03 +0500

    add: vmess hosts parse

--- src/Models/Hosts/VMessTCP.php
+++ src/Models/Hosts/VMessTCP.php
@@ -6,6 +6,21 @@ use AlexFedosienko\MarzbanAPI\Models\Host;
 
 class VMessTCP extends Host
 {
+    public string $remark;
+    public string $address;
+    public ?string $port;
+    public ?string $sni;
+    public ?string $host;
+    public ?string $path;
+    public ?string $security;
+    public ?string $alpn;
+    public ?string $fingerprint;
+    public ?string $allowinsecure;
+    public ?string $is_disabled;
+    public ?string $mux_enable;
+    public ?string $fragment_setting;
+    public ?string $random_user_agent;
+
     /**
      * make
      *
@@ -14,7 +29,23 @@ class VMessTCP extends Host
      */
     public static function make(array $hostJson): Host
     {
-        throw new \Exception('Not implemented');
+        $host = new VMessTCP();
+        $host->remark = $hostJson['remark'];
+        $host->address = $hostJson['address'];
+        $host->port = $hostJson['port'];
+        $host->sni = $hostJson['sni'];
+        $host->host = $hostJson['host'];
+        $host->path = $hostJson['path'];
+        $host->security = $hostJson['security'];
+        $host->alpn = $hostJson['alpn'];
+        $host->fingerprint = $hostJson['fingerprint'];
+        $host->allowinsecure = $hostJson['allowinsecure'];
+        $host->is_disabled = $hostJson['is_disabled'];
+        $host->mux_enable = $hostJson['mux_enable'];
+        $host->fragment_setting = $hostJson['fragment_setting'];
+        $host->random_user_agent = $hostJson['random_user_agent'];
+
+        return $host;
     }
 
     /**
@@ -28,6 +59,21 @@ class VMessTCP extends Host
      */
     public function toUrl(string $secret, string $afterRemark = '', array $params = []): string
     {
-        throw new \Exception('Not implemented');
+        $data = array_merge([
+            'add' => $this->address,
+            'aid' => '0',
+            'host' => $this->host,
+            'id' => $secret,
+            'net' => 'tcp',
+            'path' => $this->path,
+            'port' => $this->port,
+            'ps' => 'vmess',
+            'scy' => 'auto',
+            'tls' => 'none',
+            'type' => 'http',
+            'v' => '2'
+        ], $params);
+
+        return 'vmess://' . base64_encode(json_encode($data));
     }
 }
--- src/Models/Hosts/VMessWebsocket.php
+++ src/Models/Hosts/VMessWebsocket.php
@@ -6,6 +6,21 @@ use AlexFedosienko\MarzbanAPI\Models\Host;
 
 class VMessWebsocket extends Host
 {
+    public string $remark;
+    public string $address;
+    public ?string $port;
+    public ?string $sni;
+    public ?string $host;
+    public ?string $path;
+    public ?string $security;
+    public ?string $alpn;
+    public ?string $fingerprint;
+    public ?string $allowinsecure;
+    public ?string $is_disabled;
+    public ?string $mux_enable;
+    public ?string $fragment_setting;
+    public ?string $random_user_agent;
+
     /**
      * make
      *
@@ -14,8 +29,25 @@ class VMessWebsocket extends Host
      */
     public static function make(array $hostJson): Host
     {
-        throw new \Exception('Not implemented');
+        $host = new VMessWebsocket();
+        $host->remark = $hostJson['remark'];
+        $host->address = $hostJson['address'];
+        $host->port = $hostJson['port'];
+        $host->sni = $hostJson['sni'];
+        $host->host = $hostJson['host'];
+        $host->path = $hostJson['path'];
+        $host->security = $hostJson['security'];
+        $host->alpn = $hostJson['alpn'];
+        $host->fingerprint = $hostJson['fingerprint'];
+        $host->allowinsecure = $hostJson['allowinsecure'];
+        $host->is_disabled = $hostJson['is_disabled'];
+        $host->mux_enable = $hostJson['mux_enable'];
+        $host->fragment_setting = $hostJson['fragment_setting'];
+        $host->random_user_agent = $hostJson['random_user_agent'];
+
+        return $host;
     }
+
 
     /**
      * toUrl
@@ -28,6 +60,21 @@ class VMessWebsocket extends Host
      */
     public function toUrl(string $secret, string $afterRemark = '', array $params = []): string
     {
-        throw new \Exception('Not implemented');
+        $data = array_merge([
+            'add' => $this->address,
+            'aid' => '0',
+            'host' => $this->host,
+            'id' => $secret,
+            'net' => 'ws',
+            'path' => $this->path,
+            'port' => $this->port,
+            'ps' => 'ws',
+            'scy' => 'auto',
+            'tls' => 'none',
+            'type' => '',
+            'v' => '2'
+        ], $params);
+
+        return 'vmess://' . base64_encode(json_encode($data));
     }
 }
